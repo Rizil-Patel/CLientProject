65cf34bf136ff5585033e8fc70615bbcb3be717f
Rizil PATEL <128392275+Rizil-Patel@users.noreply.github.com>
2024-06-14 14:47:11 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,13 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "mygroup-rizil-patels-projects.vercel.app",
+        methods: "GET, POST, PUT, DELETE",
+        credentials: true,
+    }
+));
 app.use(express.json());
 
 dotenv.config();
